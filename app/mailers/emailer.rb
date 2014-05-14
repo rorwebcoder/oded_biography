cac8e2b8f38e18dac83cee26e60ce02f1731adcb
udhaya <ror.webcoder@gmail.com>
2014-05-13 17:42:54 -0700

    changes in email

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -4,7 +4,7 @@ class Emailer < ActionMailer::Base
   def contact_email(from_name,from_email,from_subject,from_message)
     @from_name = from_name
     @from_message = from_message
-    mail(from: from_email, to: "udhayakumar.dhanabalan@gmail.com", subject: from_subject)
+    mail(from: from_email, to: "korczyn@gmail.com", subject: from_subject)
   end
   
 end
